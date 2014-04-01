e44794e2ef964fd6684d4a83145c490ef957c342
Carlos Huga <Carlospow279@gmail.com>
2014-04-01 09:19:16 -0430

    Se agregaron los archivos .exe al repositorio por primera vez

---
+++ config.py
@@ -0,0 +1,18 @@
+import datetime 
+params ={   'cancelinvoices': '/api/cancelinvoice.json',
+    'clients': '/api/clients.json',
+    'errors': '/api/logs.json',
+    'init': 1,
+    'interval': '1800',
+    'invoices': '/api/invoices.json',
+    'log_error_file': 'errors.log',
+    'log_file': 'loader.log',
+    'log_size': 2,
+    'login': '/api/login.json',
+    'no_id': ['01'],
+    'owners': '/api/owners.json',
+    'port': '3000',
+    'products': '/api/products.json',
+    'time_init': datetime.datetime(2014, 3, 31, 12, 31, 45, 643632),
+    'time_load': datetime.datetime(2014, 3, 31, 12, 29, 0, 250172),
+    'url': 'http://192.168.0.151'}

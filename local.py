404c1d3c14960b921b85176b2617ba338d237cd8
Carlos Huga <Carlospow279@gmail.com>
2014-04-01 09:38:38 -0430

    Se agregaron los archivos exe por primera vez

---
+++ local.py
@@ -0,0 +1,7 @@
+local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
+    'id': 'Wok01',
+    'legal_name': 'On the wok',
+    'name': 'On the wok Altamira',
+    'rif': 'J-1234asdas0-8'}
+configs ={   'db': 'c:\\WOK\\OnTheWok2k.mdb',
+    'driver': '{Microsoft Access Driver (*.mdb, *.accdb)}'}

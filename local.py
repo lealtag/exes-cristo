dee981c75bbf0c3ac30d5282d2cd5d39e5a1181d
Carlos Huga <Carlospow279@gmail.com>
2014-04-07 09:14:02 -0430

    Correccion de claves y de datos del api

--- local.py
+++ local.py
@@ -1,2 +1,10 @@
-local ={'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA', 'id': 'Froyo01', 'name': 'Froyogur Centro Plaza', 'legal_name': 'Inversiones Centro Plaza C.A', 'rif': 'J-1234566000-8'}
-configs ={'server': 'localhost\\SQLEXPRESS', 'driver': '{SQL Server}', 'user': 'x_connector', 'db': 'Froyogur', 'password': 'serio'}
+local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PARQUE CRISTAL',
+    'id': 'Froyo02',
+    'legal_name': 'Froyo Parque Cristal',
+    'name': 'Froyogur Parque Cristal',
+    'rif': 'J-1234566000-8'}
+configs ={   'db': 'FroyogurCristal',
+    'driver': '{SQL Server}',
+    'password': 'serio',
+    'server': 'localhost\\SQLEXPRESS',
+    'user': 'x_connector'}

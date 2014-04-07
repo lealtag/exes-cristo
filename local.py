9188b10ba7c5e6ad055b9693b5d267c8ac8cc947
Carlos Huga <Carlospow279@gmail.com>
2014-04-07 17:20:46 -0430

    Correcciones en la clave, se agregaron los datos del cliente

--- local.py
+++ local.py
@@ -1,2 +1,2 @@
 local ={'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA', 'id': 'Froyo01', 'name': 'Froyogur Centro Plaza', 'legal_name': 'Inversiones Centro Plaza C.A', 'rif': 'J-1234566000-8'}
-configs ={'server': 'localhost\\SQLEXPRESS', 'driver': '{SQL Server}', 'user': 'x_connector', 'db': 'Froyogur', 'password': 'serio'}
+configs ={'password': 'L34lT4g-connect', 'user': 'lealtag_client', 'driver': '{SQL Server}', 'server': 'localhost\\FRO', 'db': 'AdminFro'}

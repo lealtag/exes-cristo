6283e4d6ce25eacb0708fa6860458bbb2e3c4532
Carlos Huga <Carlospow279@gmail.com>
2014-04-29 17:01:03 -0430

    Stopper 0, Init 0, 01-04-2014

--- local.py
+++ local.py
@@ -1,7 +1,7 @@
 local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
     'id': 'Wok01',
     'legal_name': 'On the wok',
-    'name': 'On the wok Altamira',
+    'name': 'On the wok, Los Palos Grandes',
     'rif': 'J-1234asdas0-8'}
 configs ={   'db': 'D:\\red\\dat\\OnTheWok2k.mdb',
     'driver': '{Microsoft Access Driver (*.mdb, *.accdb)}'}

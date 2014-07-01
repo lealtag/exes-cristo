def4399c7a2e3155eeca5c590f77564dd7a0fe0c
Carlos Huga <Carlospow279@gmail.com>
2014-07-01 16:48:38 -0430

    Primer commit shibuya

---
+++ local.py
@@ -0,0 +1,8 @@
+local ={   'address': 'SAN LUIS',
+    'id': 'Shibuya01',
+    'legal_name': 'Shibuya',
+    'name': 'Shibuya',
+    'no_id': ['1'],
+    'rif': 'J-1234asdas0-8'}
+configs ={   'dbfilepattern': '*.DBF',
+    'dbfolder': 'C:\\Users\\Carlos\\Documents\\MobaXterm\\home\\Nueva carpeta'}

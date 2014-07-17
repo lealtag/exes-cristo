e88f96370ec290d0c3520c3deb4be0ecad9e01b9
Carlos Huga <Carlospow279@gmail.com>
2014-07-17 09:53:09 -0430

    Primer Commit

---
+++ local.py
@@ -0,0 +1,10 @@
+local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
+    'id': 'Hamburgueseria01',
+    'legal_name': 'Inversiones Centro Plaza C.A',
+    'name': 'Hamburgueseria 212',
+    'rif': 'J-1234566000-8'}
+configs ={   'db': 'Hamburgueseria',
+    'driver': '{SQL Server}',
+    'password': '1234',
+    'server': 'localhost\\SQLExpress',
+    'user': 'x_connector'}

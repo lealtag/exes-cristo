226972d9eb62afa430cc023d0bc919318777c69e
Carlos Huga <Carlospow279@gmail.com>
2014-07-21 17:08:23 -0430

    Correcciones en el stopper y el usuario

--- local.py
+++ local.py
@@ -3,8 +3,8 @@ local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
     'legal_name': 'Inversiones Centro Plaza C.A',
     'name': 'Hamburgueseria 212',
     'rif': 'J-1234566000-8'}
-configs ={   'db': 'Hamburgueseria',
+configs ={   'db': 'AdminColectiva',
     'driver': '{SQL Server}',
     'password': '1234',
-    'server': 'localhost\\SQLExpress',
+    'server': 'COLECTIVA',
     'user': 'x_connector'}

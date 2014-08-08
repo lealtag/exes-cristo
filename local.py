c02c78970b463e9622b64da29173377a6025dee4
Carlos Huga <Carlospow279@gmail.com>
2014-08-08 10:21:38 -0430

    STOPPER 0

--- local.py
+++ local.py
@@ -1,10 +1,10 @@
-local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
+local ={   'address': 'LAS MERCEDES',
     'id': 'Yogurfit01',
     'legal_name': 'Inversiones heladitos magicos',
-    'name': 'Ypgurfit KR7',
+    'name': 'Yogurfit KR7',
     'rif': 'J-1234566000-8'}
 configs ={   'db': 'micros',
     'driver': '{SQL Server}',
     'password': 'micros3700',
-    'server': 'sqlLT-1',
+    'server': 'MICROSSERVER',
     'user': 'dba'}

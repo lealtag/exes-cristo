e3d16d50d6a609363269ca3ea9b7cbcce6b6320e
Carlos Huga <Carlospow279@gmail.com>
2014-08-08 08:27:09 -0430

    Primer commit de este branch

--- local.py
+++ local.py
@@ -1,10 +1,10 @@
 local ={   'address': 'AV. FRANCISCO DE MIRANDA, CENTRO COMERCIAL PLAZA',
-    'id': 'Hamburgueseria01',
-    'legal_name': 'Inversiones Centro Plaza C.A',
-    'name': 'Hamburgueseria 212',
+    'id': 'Yogurfit01',
+    'legal_name': 'Inversiones heladitos magicos',
+    'name': 'Ypgurfit KR7',
     'rif': 'J-1234566000-8'}
-configs ={   'db': 'AdminColectiva',
+configs ={   'db': 'micros',
     'driver': '{SQL Server}',
-    'password': '1234',
-    'server': 'COLECTIVA',
-    'user': 'x_connector'}
+    'password': 'micros3700',
+    'server': 'sqlLT-1',
+    'user': 'dba'}

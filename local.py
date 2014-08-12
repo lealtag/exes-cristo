bf97701cbc0e370bd33a2883638c62b22525c2cb
Carlos Huga <Carlospow279@gmail.com>
2014-08-12 14:47:00 -0430

    Correccion en el loader

--- local.py
+++ local.py
@@ -1,7 +1,7 @@
 local ={   'address': 'CENTRO COMERCIAL GALERIAS LOS NARANJOS',
     'id': 'Artegelato01',
     'legal_name': 'Xoco Venezuela - Los Naranjos',
-    'name': 'Xoco Venezuela,
+    'name': 'Xoco Venezuela',
     'no_id': [''],
     'rif': 'J-1234asdas0-8'}
 configs ={   'dbfilepattern': '*.DBF',

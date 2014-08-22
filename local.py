883a8df72aa4f8e3e73afc74f0c5bbec04fe69e6
Carlos Huga <Carlospow279@gmail.com>
2014-08-22 12:30:28 -0430

    Primer Commit

--- local.py
+++ local.py
@@ -1,7 +1,7 @@
-local ={   'address': 'CENTRO COMERCIAL EL RECREO, NIVEL PLAZA',
-    'id': 'Artegelato04',
-    'legal_name': 'Artegelato - C.C. El Recreo N. Plaza',
-    'name': 'Artegelato - C.C. El Recreo N. Plaza',
+local ={   'address': 'CENTRO COMERCIAL COSTA AZUL',
+    'id': 'Artegelato05',
+    'legal_name': 'Artegelato - C.C. Costa Azul',
+    'name': 'Artegelato - C.C. Costa Azul',
     'no_id': [''],
     'rif': 'J-1234asdas0-8'}
 configs ={   'dbfilepattern': '*.DBF',

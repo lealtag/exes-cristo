1fc571c2a30c7e3ece139fe863ae0222b01f1d4f
Carlos Huga <Carlospow279@gmail.com>
2014-09-12 09:39:38 -0430

    Primer commit

--- local.py
+++ local.py
@@ -1,10 +1,10 @@
 local ={   'address': 'CENTRO COMERCIAL XPRESS MACARACUAY',
-    'id': 'Yogurfit03',
+    'id': 'Yogurfit04',
     'legal_name': 'Inversiones heladitos magicos',
     'name': 'Ypgurfit Xpress Macaracuay',
     'rif': 'J-1234566000-8'}
 configs ={   'db': 'micros',
     'driver': '{SQL Server}',
     'password': 'micros3700',
-    'server': 'sqlMICROS-HP',
+    'server': 'sqlMICROSSVC',
     'user': 'dba'}
